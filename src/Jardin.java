cd401b15e41a21173c46d9b6ee5ee669f6b17520
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-06-02 17:40:51 +0200

    Lecture du nom
    * Créer un objet de type Scanner.
    * Utiliser l'objet pour lire une chaîne caractère d'entrée standard.
    * Fermer le scanner à la fin de l'exécusion.

--- src/Jardin.java
+++ src/Jardin.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Classe principale.
  */
@@ -10,5 +12,12 @@ public class Jardin {
         // Arroser la plante
         taille += 0.25;
         System.out.println("Taille après l'arrosage: " + taille + " cm.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Comment s'appelle-t-elle votre plante?");
+        String nom = scanner.nextLine();
+        System.out.println(nom + " vous remercie de l'avoir arrosé.");
+
+        scanner.close();
     }
 }
