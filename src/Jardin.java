457739719fb4082167c60f5895231fbae1130078
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-06-02 18:24:49 +0200

    Boucler tant erreur
    * Boucler avec message d'erreur tant qu'un entier n'est pas renseigné.
    * Utiliser une structure `while`.

--- src/Jardin.java
+++ src/Jardin.java
@@ -16,14 +16,14 @@ public class Jardin {
 
         // Arroser la plante
         System.out.println("Combien de fois voulez-vous arroser la plante?");
-        if (scanner.hasNextInt()) {
-            int n = scanner.nextInt();
-            taille += n * 0.25;
-            System.out.println("Taille après l'arrosage: " + taille + " cm.");
-            System.out.println(nom + " vous remercie de l'avoir arrosé.");
-        } else {
-            System.out.println("Désolé, je n'ai pas compris. Je m'attendais à un chiffre entier.");
+        while (!scanner.hasNextInt()) {
+            scanner.nextLine();
+            System.out.println("Désolé, je n'ai pas compris. Veuillez renseigner à un chiffre entier.");
         }
+        int n = scanner.nextInt();
+        taille += n * 0.25;
+        System.out.println("Taille après l'arrosage: " + taille + " cm.");
+        System.out.println(nom + " vous remercie de l'avoir arrosé.");
 
         scanner.close();
     }
