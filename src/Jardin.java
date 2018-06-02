82e61a282a2897251abc3e11c4575ddae5b5e42c
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-06-02 12:09:18 +0200

    Afficher la taille de plante
    * Déclarer une variable.
    * Commentaire à la fin de la ligne.
    * Afficher un message sans retour à la ligne.
    * Afficher une variable.

--- src/Jardin.java
+++ src/Jardin.java
@@ -5,5 +5,8 @@ public class Jardin {
 
     public static void main(String[] args) {
         System.out.println("Bonjour Jardin!");
+        int taille = 30; // Taille de plante
+        System.out.print("Taille initiale de plante: ");
+        System.out.println(taille);
     }
 }
