2f37555e9afd3f843263fbd46b45ab01436e3125
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-06-02 18:33:05 +0200

    Choix de taille en cm ou en pouce
    * Utiliser l'operation de comparaison `==`.
    * Utiliser la structure `if`/`else if`/`else`.

--- src/Jardin.java
+++ src/Jardin.java
@@ -22,7 +22,18 @@ public class Jardin {
         }
         int n = scanner.nextInt();
         taille += n * 0.25;
-        System.out.println("Taille après l'arrosage: " + taille + " cm.");
+
+        System.out.println("Quelle unité voulez-vous utiliser pour l'affichage?");
+        System.out.println("Renseigner 1 pour CM et 2 pour pouce.");
+        int unite = scanner.nextInt();
+        if (unite == 1) {
+            System.out.println("Taille après l'arrosage: " + taille + " cm.");
+        } else if (unite == 2) {
+            double tailleEnPouce = taille * 0.393701;
+            System.out.println("Taille après l'arrosage: " + tailleEnPouce + " pouce.");
+        } else {
+            System.out.println("Désolé, je n'ai pas compris votre choix.");
+        }
         System.out.println(nom + " vous remercie de l'avoir arrosé.");
 
         scanner.close();
