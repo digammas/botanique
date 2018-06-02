4e8b930b36023675026710eab07f4e50456c0c21
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-06-02 18:01:32 +0200

    Arrosage conditionnel
    * Traiter uniquement le cas où un entier a été renseigné.
    * Utiliser une structure `if`.

--- src/Jardin.java
+++ src/Jardin.java
@@ -16,11 +16,12 @@ public class Jardin {
 
         // Arroser la plante
         System.out.println("Combien de fois voulez-vous arroser la plante?");
-        int n = scanner.nextInt();
-        taille += n * 0.25;
-        System.out.println("Taille après l'arrosage: " + taille + " cm.");
-
-        System.out.println(nom + " vous remercie de l'avoir arrosé.");
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            taille += n * 0.25;
+            System.out.println("Taille après l'arrosage: " + taille + " cm.");
+            System.out.println(nom + " vous remercie de l'avoir arrosé.");
+        }
 
         scanner.close();
     }
