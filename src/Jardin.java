873caf00fa90d51a084f0e180cccd172066d95c5
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-06-02 17:58:22 +0200

    Lecture de nombre d'arrosage
    * Utiliser l'objet scanner pour lire un entier.
    * Attention, une erreur d'exécusion si on ne renseigne  pas un entier.

--- src/Jardin.java
+++ src/Jardin.java
@@ -9,13 +9,17 @@ public class Jardin {
         System.out.println("Bonjour Jardin!");
         double taille = 30; // Taille de plante en CM
         System.out.println("Taille initiale de plante: " + taille + " cm.");
-        // Arroser la plante
-        taille += 0.25;
-        System.out.println("Taille après l'arrosage: " + taille + " cm.");
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Comment s'appelle-t-elle votre plante?");
         String nom = scanner.nextLine();
+
+        // Arroser la plante
+        System.out.println("Combien de fois voulez-vous arroser la plante?");
+        int n = scanner.nextInt();
+        taille += n * 0.25;
+        System.out.println("Taille après l'arrosage: " + taille + " cm.");
+
         System.out.println(nom + " vous remercie de l'avoir arrosé.");
 
         scanner.close();
