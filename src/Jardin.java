11f8c44b3813a31eb9fdbf01432a1e5e952880af
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-06-02 16:50:01 +0200

    Taille plus précise
    * Utiliser une variable de type double.
    * L'operation d'incrémentation.

--- src/Jardin.java
+++ src/Jardin.java
@@ -5,10 +5,10 @@ public class Jardin {
 
     public static void main(String[] args) {
         System.out.println("Bonjour Jardin!");
-        int taille = 30; // Taille de plante en CM
+        double taille = 30; // Taille de plante en CM
         System.out.println("Taille initiale de plante: " + taille + " cm.");
         // Arroser la plante
-        taille++;
+        taille += 0.25;
         System.out.println("Taille après l'arrosage: " + taille + " cm.");
     }
 }
