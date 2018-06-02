794cc572988afd76575c22e5e38d9d201ba17951
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-06-02 12:18:15 +0200

    Calculer la taille après l'arrosage
    * Additionner deux entier.

--- src/Jardin.java
+++ src/Jardin.java
@@ -8,5 +8,8 @@ public class Jardin {
         int taille = 30; // Taille de plante
         System.out.print("Taille initiale de plante: ");
         System.out.println(taille);
+        System.out.print("Taille après l'arrosage: ");
+        taille = taille + 1;
+        System.out.println(taille);
     }
 }
