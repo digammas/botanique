495d99aa0b5ef05a856a6a442d66305cd2282d1b
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-06-03 16:50:21 +0200

    Deux nouvelles classe de plante
    * Créer une sous-classe en utilisant le mot clef `extends`.
    * Faire appel au constructeur parent en utilisant le mot clef `super`.
    * Créer des objets des nouvelles classes.

---
+++ src/com/acme/botanique/Basilic.java
@@ -0,0 +1,14 @@
+package com.acme.botanique;
+
+/**
+ * Une plante de basilic.
+ */
+public class Basilic extends Plante {
+
+    /**
+     * Constructeur sans argument.
+     */
+    public Basilic() {
+        super(45);
+    }
+}
---
+++ src/com/acme/botanique/Geranium.java
@@ -0,0 +1,14 @@
+package com.acme.botanique;
+
+/**
+ * Une plante de Géranium.
+ */
+public class Geranium extends Plante {
+
+    /**
+     * Constructeur sans argument.
+     */
+    public Geranium() {
+        super(20);
+    }
+}
