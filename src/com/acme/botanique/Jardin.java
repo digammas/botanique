9366a1eebd944aae478efd5143eb38f27a27245c
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-06-03 15:05:53 +0200

    Déclarer mon jardin comme objet
    * Transformer la classe Jardin à une classe avec état.
    * Créer une instance de type `Jardin`.
    * Ajouter des commentaires.
    * Ajouter une méthode publique pour accéder aux plantes.

--- src/com/acme/botanique/Jardin.java
+++ src/com/acme/botanique/Jardin.java
@@ -9,13 +9,53 @@ import java.util.Scanner;
  */
 public class Jardin {
 
-    private static Plante[] plantes = new Plante[] {new Plante(), new Plante(20), new Plante(45)};
+    /**
+     * Toutes les plantes dans le jardin.
+     */
+    private Plante[] plantes = new Plante[] {new Plante(), new Plante(20), new Plante(45)};
 
+    /**
+     * L'enseble des plantes du jardin.
+     * 
+     * @return  les plante en tant que tableau
+     */
+    public Plante[] lirePlantes() {
+        return plantes;
+    }
+
+    /**
+     * Arroser toutes les plantes, n fois.
+     *
+     * @param n     nombre de fois d'arrosage
+     */
+    public void arroser(int n) {
+        for (Plante plante : plantes) {
+            plante.arroser(n);
+        }
+    }
+
+    /**
+     * Afficher toutes les plantes.
+     *
+     * @param unite     unité utilisée pour l'affichage de taille
+     */
+    public void afficher(String unite) {
+        for (Plante plante : plantes) {
+            afficher(plante.lireTaille(), unite);
+        }
+    }
+
+    /**
+     * Point d'entrée de l'application.
+     *
+     * @param args  liste d'arguments
+     */
     public static void main(String[] args) {
         System.out.println("Bonjour Jardin!");
 
         Scanner scanner = new Scanner(System.in);
 
+        Jardin monJardin = new Jardin();
         // Arroser la plante
         System.out.println("Combien de fois voulez-vous arroser la plante?");
         while (!scanner.hasNextInt()) {
@@ -24,29 +64,23 @@ public class Jardin {
         }
         int n = scanner.nextInt();
 
-        arroser(n);
+        monJardin.arroser(n);
 
         System.out.println("Quelle unité voulez-vous utiliser pour l'affichage?");
         String unite = scanner.next();
 
-        afficher(unite);
+        monJardin.afficher(unite);
         System.out.println("Vos plantes vous remercient de les avoir arrosé.");
 
         scanner.close();
     }
 
-    private static void arroser(int n) {
-        for (Plante plante : plantes) {
-            plante.arroser(n);
-        }
-    }
-
-    private static void afficher(String unite) {
-        for (Plante plante : plantes) {
-            afficher(plante.lireTaille(), unite);
-        }
-    }
-
+    /**
+     * Méthode utilitaire pour afficher la taille après l'arrosage dans une unité donnée.
+     *
+     * @param taille        la taille
+     * @param unite         l'unité
+     */
     private static void afficher(double taille, String unite) {
         switch (unite.toLowerCase()) {
         case "cm":
