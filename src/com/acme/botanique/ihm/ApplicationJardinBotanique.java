e1483587cd6a7d22961472ad3dc3e1428e099dcc
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2018-09-02 18:28:45 +0200

    Ré-usinage du code pour une meilleure lisibilité
    * Sortir la création de certains contrôle dans des méthodes apart.
    * Une méthode pour gérer l'affichage des plantes sur la grille.

--- src/com/acme/botanique/ihm/ApplicationJardinBotanique.java
+++ src/com/acme/botanique/ihm/ApplicationJardinBotanique.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.GridPane;
@@ -18,6 +17,11 @@ import javafx.stage.Stage;
  * Interface utilisateur graphique pour le jardin botanique.
  */
 public class ApplicationJardinBotanique extends Application {
+
+    /**
+     * Une grille d'affichage.
+     */
+    private GridPane grille;
 
     /**
      * Point d'entré de l'interface graphique.
@@ -37,49 +41,19 @@ public class ApplicationJardinBotanique extends Application {
         // Création d'un label bienvenue
         Label labelBienvenue = new Label("Bienvenue dans ce jardin botanique.");
         // Création d'une mise en page de type pile verticale
-        VBox pile = new VBox();
-        // Centraliser la mise en page
-        pile.setAlignment(Pos.CENTER);
-        // Définir l'espacement verticale de la pile
-        pile.setSpacing(20);
+        VBox pile = creerPile();
         // Ajout du label bienvenue
         pile.getChildren().add(labelBienvenue);
         // Création d'un panneau avec une mise en page de type grille
-        GridPane grille = new GridPane();
-        // Centraliser les éléments à l'interieur du panneau
-        grille.setAlignment(Pos.CENTER);
-        // Définir l'espacement horizontal
-        grille.setHgap(10);
-        // Définir l'espacement vertical
-        grille.setVgap(10);
-        // Définir un compteur de lignes
-        int ligne = 0;
-        for (Plante plante: monJardin.lirePlantes()) {
-            // Création d'un label contenant le nom de la plante
-            Text texteNom = new Text(plante.lireNom());
-            // Sous-ligner le nom de la plante
-            texteNom.setUnderline(true);
-            // Création d'un label contenant la représentation de la plante
-            Text textePlante = new Text(plante.toString());
-            // Ajout du nom de la plante à la première colonne
-            grille.add(texteNom, 0, ligne);
-            // Ajout de la représentation de la plante à la deuxième colonne
-            grille.add(textePlante, 1, ligne);
-            // Augmentation du numéro de ligne
-            ligne++;
-        }
+        this.grille = creerGrille();
+        // Afficher les plantes
+        afficherPlante(monJardin);
         // Ajouter la grille à la fin de pile
         pile.getChildren().add(grille);
         // Création d'un bouton d'arrosage
         Button boutonArrosage = new Button("Arroser");
         // Création d'une boîte de dialogue
-        TextInputDialog dialogueArrosage = new TextInputDialog();
-        // Définir le titre de la boîte
-        dialogueArrosage.setTitle("Arrosage des plante");
-        // Définir le texte du message
-        dialogueArrosage.setContentText("Quantité d'arrosage");
-        // Le dialogue n'a pas d'entête
-        dialogueArrosage.setHeaderText(null);
+        TextInputDialog dialogueArrosage = creerDialogueArrosage();
         // Définition de l'acction du bouton : afficher le dialogue d'arrosage
         boutonArrosage.setOnAction(event -> dialogueArrosage.showAndWait());
         // Ajout du bouton à la fin de pile
@@ -95,4 +69,76 @@ public class ApplicationJardinBotanique extends Application {
         // Affichage de la fenêtre
         primaryStage.show();
     }
+
+    /**
+     * Afficher les plantes du jardin sur la grille d'affichage.
+     *
+     * @param jardin    le jardin contenant des plantes
+     */
+    private void afficherPlante(Jardin jardin) {
+        // Définir un compteur de lignes, on commence à la première ligne
+        int ligne = 0;
+        for (Plante plante: jardin.lirePlantes()) {
+            // Création d'un label contenant le nom de la plante
+            Text texteNom = new Text(plante.lireNom());
+            // Sous-ligner le nom de la plante
+            texteNom.setUnderline(true);
+            // Création d'un label contenant la représentation de la plante
+            Text textePlante = new Text(plante.toString());
+            // Ajout du nom de la plante à la première colonne
+            this.grille.add(texteNom, 0, ligne);
+            // Ajout de la représentation de la plante à la deuxième colonne
+            this.grille.add(textePlante, 1, ligne);
+            // Augmentation du numéro de ligne
+            ligne++;
+        }
+    }
+
+    /**
+     * Créer un panneau avec une mise en page type pile verticale
+     *
+     * @return  une pile d'affichage
+     */
+    private static VBox creerPile() {
+        VBox pile = new VBox();
+        // Centraliser la mise en page
+        pile.setAlignment(Pos.CENTER);
+        // Définir l'espacement verticale de la pile
+        pile.setSpacing(20);
+        return pile;
+    }
+
+    /**
+     * Créer un panneau avec une mise en page type grille.
+     *
+     * @return  une grille d'affichage
+     */
+    private static GridPane creerGrille() {
+        GridPane grille = new GridPane();
+        // Centraliser les éléments à l'interieur du panneau
+        grille.setAlignment(Pos.CENTER);
+        // Définir l'espacement horizontal
+        grille.setHgap(10);
+        // Définir l'espacement vertical
+        grille.setVgap(10);
+        return grille;
+    }
+
+
+    /**
+     * Créer une boîte de dialogue d'arrosage.
+     *
+     * @return  boîte de dialoge d'arrosage
+     */
+    private static TextInputDialog creerDialogueArrosage() {
+        TextInputDialog dialogueArrosage = new TextInputDialog();
+        // Définir le titre de la boîte
+        dialogueArrosage.setTitle("Arrosage des plante");
+        // Définir le texte du message
+        dialogueArrosage.setContentText("Quantité d'arrosage");
+        // Le dialogue n'a pas d'entête
+        dialogueArrosage.setHeaderText(null);
+        return dialogueArrosage;
+    }
+
 }
